2232c53664a45ac79f858418f77294ce264c8be2
vampire321 <mh1295551@gmail.com>
2026-02-18 16:15:15 +0530

    added delete  function

--- CRUD.go
+++ CRUD.go
@@ -73,4 +73,18 @@ func updateUser(c *gin.Context){
 	}
 	c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 }
+func deleteUser(c *gin.Context){
+	id,_ := strconv.Atoi(c.Param("id"))
+	for i,u := range users{
+		if u.ID == id {
+			users = append(users[:i],users[i+1:]...)
+			c.JSON(http.StatusOK,gin.H{"message":"User deleted"})
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound,gin.H{
+		"error":"User not found",
+	})
+
+}
 
